662a8468e13ee83bf4e9016f779692ee0f9c1145
farias-77 <gabrielfalves2003@gmail.com>
2022-08-03 15:14:35 -0300

    adiciona /get em /urls/:id

--- src/controllers/urlsControllers.js
+++ src/controllers/urlsControllers.js
@@ -1,7 +1,7 @@
 import connection from "../database/databaseConnection.js";
 import { nanoid } from "nanoid";
 
-export async function returnShortUrl(req, res){
+export async function postShortUrl(req, res){
     try{
         const { url } = req.body;
         const userId = res.locals.user.userId;
@@ -13,6 +13,26 @@ export async function returnShortUrl(req, res){
         `,[url, shortUrl, userId]);
 
         return res.send({ shortUrl });
+    }catch{
+        return res.status(500).send("Ocorreu um erro inesperado, tente novamente por favor.");
+    }
+}
+
+export async function getShortUrlById(req, res){
+    try{
+        const { id } = req.params;
+
+        const { rowCount, rows: shortUrl } = await connection.query(`
+            SELECT "id", "shortUrl", "url"
+            FROM urls
+            WHERE id = $1
+        `, [id]);
+
+        if(rowCount === 0){
+            return res.status(404).send("Não existe uma url com o id fornecido.");
+        }
+
+        return res.status(200).send(shortUrl[0]);
     }catch{
         return res.status(500).send("Ocorreu um erro inesperado, tente novamente por favor.");
     }
--- src/routes/urlsRouter.js
+++ src/routes/urlsRouter.js
@@ -1,9 +1,10 @@
 import express from "express";
 import { urlBodyValidation, tokenValidation, repeatedUrlValidation } from "../middlewares/urlsMiddlewares.js";
-import { returnShortUrl } from "../controllers/urlsControllers.js";
+import { postShortUrl, getShortUrlById } from "../controllers/urlsControllers.js";
 
 const router = express.Router();
 
-router.post("/urls/shorten", urlBodyValidation, tokenValidation, repeatedUrlValidation, returnShortUrl);
+router.post("/urls/shorten", urlBodyValidation, tokenValidation, repeatedUrlValidation, postShortUrl);
+router.get("/urls/:id", getShortUrlById)
 
 export default router;
