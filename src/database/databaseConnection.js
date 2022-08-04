a8d95556194b2cedec7036817ad08141ecd198f2
farias-77 <gabrielfalves2003@gmail.com>
2022-08-04 15:26:08 -0300

    adiciona configurações para o deploy no heroku

--- src/database/databaseConnection.js
+++ src/database/databaseConnection.js
@@ -1,13 +1,14 @@
-import pg from "pg";
+import pg from 'pg';
 
 const { Pool } = pg;
 
-const connection = new Pool({
-    user: 'postgres',
-    host: 'localhost',
-    port: 5432,
-    database: 'shortly',
-    password: '15032003'
-});
+const databaseConfig = {
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+        rejectUnauthorized: false
+    }
+}
+
+const connection = new Pool(databaseConfig);
 
 export default connection;
